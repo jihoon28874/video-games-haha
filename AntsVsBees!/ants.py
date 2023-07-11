e8f7e77b39e793c858bb89176297c2ee6943abc3
Jihoon Lee <jihoon123@berkeley.edu>
2023-07-11 12:52:19 -0700

    Completed bees

--- AntsVsBees!/ants.py
+++ AntsVsBees!/ants.py
@@ -24,10 +24,9 @@ class Place:
         self.bees = []        # A list of Bees
         self.ant = None       # An Ant
         self.entrance = None  # A Place
-        # Phase 1: Add an entrance to the exit
-        # BEGIN Problem 2
-        "*** YOUR CODE HERE ***"
-        # END Problem 2
+        # Add an entrance to the exit
+        if self.exit is not None:
+            self.exit.entrance = self
 
     def add_insect(self, insect):
         """
@@ -157,16 +156,12 @@ class HarvesterAnt(Ant):
 
     name = 'Harvester'
     implemented = True
-    # OVERRIDE CLASS ATTRIBUTES HERE
+    food_cost = 2
 
     def action(self, gamestate):
         """Produce 1 additional food for the colony.
-
-        gamestate -- The GameState, used to access game state information.
         """
-        # BEGIN Problem 1
-        "*** YOUR CODE HERE ***"
-        # END Problem 1
+        gamestate.food += 1
 
 
 class ThrowerAnt(Ant):
@@ -175,7 +170,7 @@ class ThrowerAnt(Ant):
     name = 'Thrower'
     implemented = True
     damage = 1
-    # ADD/OVERRIDE CLASS ATTRIBUTES HERE
+    food_cost = 3
 
     def nearest_bee(self):
         """Return the nearest Bee in a Place that is not the HIVE, connected to
@@ -184,7 +179,13 @@ class ThrowerAnt(Ant):
         This method returns None if there is no such Bee (or none in range).
         """
         # BEGIN Problem 3 and 4
-        return random_bee(self.place.bees)  # REPLACE THIS LINE
+        current_place = self.place
+        while not current_place.is_hive:
+            current_bee = random_bee(current_place.bees)
+            if current_bee is not None:
+                return random_bee(current_place.bees)
+            current_place = current_place.entrance
+        # REPLACE THIS LINE
         # END Problem 3 and 4
 
     def throw_at(self, target):
